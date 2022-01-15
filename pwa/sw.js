2e851fb4e6dd877b42a311b0f0cfb5a1a4c55a67
Mihai Vasilache <mihai.vasilache@gmail.com>
2022-01-15 09:23:17 +0200

    last-dojo

--- pwa/sw.js
+++ pwa/sw.js
@@ -6,7 +6,7 @@
  * webpack/webpack.common.js
  */
 import { precacheAndRoute } from 'workbox-precaching';
-
+self.__WB_DISABLE_DEV_LOGS = true;
 console.log('~~~~ Running sw.js');
 
 // Use with precache injection
