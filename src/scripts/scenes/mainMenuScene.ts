9bdbffb7655d9e8a729436368b516228b37c7b36
Mihai Vasilache <mihai.vasilache@gmail.com>
2022-01-22 09:21:21 +0200

    De la Ana

--- src/scripts/scenes/mainMenuScene.ts
+++ src/scripts/scenes/mainMenuScene.ts
@@ -1,8 +1,8 @@
 import 'phaser';
+import Hero from '../entities/hero';
 
 export default class MainMenuScene extends Phaser.Scene {
-    rightKey: Phaser.Input.Keyboard.Key;
-    hero: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    hero: Hero;
     constructor() {
         super({ key: 'MainMenuScene' });
     }
@@ -10,6 +10,7 @@ export default class MainMenuScene extends Phaser.Scene {
     preload() {
         this.load.spritesheet('idle-e-spritesheet', 'assets/hero/idle_aggro_E.png', { frameWidth: 128, frameHeight: 128 });
         this.load.spritesheet('walk-e-spritesheet', 'assets/hero/walk_aggro_E.png', { frameWidth: 128, frameHeight: 128 });
+
     }
     create() {
         // remove the loading screen
@@ -27,31 +28,12 @@ export default class MainMenuScene extends Phaser.Scene {
 
         this.cameras.main.fadeIn(2000);
         this.cameras.main.setBackgroundColor('#008080');
-        this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        this.hero = this.physics.add.sprite(200, 200, 'idle-e-spritesheet', 0);
-        this.anims.create({
-            key: 'idle-e-anim',
-            frames: this.anims.generateFrameNumbers('idle-e-spritesheet', {}),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'walk-e-anim',
-            frames: this.anims.generateFrameNumbers('walk-e-spritesheet', {}),
-            frameRate: 10,
-            repeat: -1
-        });
+        
 
-        this.hero.anims.play('idle-e-anim');
+        this.hero = new Hero(this, 200, 200);
+       
+      
     }
 
-    update(time, delta) {
-        this.hero.body.setVelocity(0);
-        if (this.rightKey.isDown) {
-            this.hero.body.setVelocityX(175);
-            this.hero.anims.play('walk-e-anim', true);
-        } else {
-            this.hero.anims.play('idle-e-anim', true);
-        }
-    }
+    update(time, delta) {}
 }
